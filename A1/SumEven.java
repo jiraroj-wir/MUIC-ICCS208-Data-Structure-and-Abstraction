8bbe50658ef37a5acacf86c58b54bd9283eda6a8
orca1018 <dev@jiraroj.net>
2026-01-09 18:12:26 +0700

    hw 1, task 2, subtask I; change to testing with assertion

--- A1/SumEven.java
+++ A1/SumEven.java
@@ -1,5 +1,10 @@
 public class SumEven {
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+        assert sumEven(0) == 0;
+        assert sumEven(1) == 0;
+        assert sumEven(4) == 12;
+        assert sumEven(10) == 90;
+    }
     /*
      * The time complexity of this function is O(n), so roughly n steps.
      */
