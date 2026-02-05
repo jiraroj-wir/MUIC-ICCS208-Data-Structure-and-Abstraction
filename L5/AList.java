6dfeb273ea77e4a8c2410da8130fcacbd2b3a76f
orca1018 <dev@jiraroj.net>
2026-02-05 15:18:04 +0700

    lecture 5, exercise 2; will test next

--- L5/AList.java
+++ L5/AList.java
@@ -1,14 +1,14 @@
-// public class AList<T> {
-public class AList {
-    // private T[] items;
-    private int[] items;
+public class AList<T> {
+    // public class AList {
+    private T[] items;
+    // private int[] items;
     private int size;
 
     // Creates an empty array deque.
     @SuppressWarnings("unchecked") // remove my lsp warnings
     public AList() {
-        // items = (T[]) new Object[8];
-        items = new int[8];
+        items = (T[]) new Object[8];
+        // items = new int[8];
 
         this.size = 0;
     }
@@ -28,7 +28,7 @@ public class AList {
     But, for a circular array based list, since we kept track of the list's head and tail, the front append time would
     be constant --- O(1).
     */
-    public void addFirst(int x) {
+    public void addFirst(T x) {
         if (this.size() == this.items.length) {
             resize(this.items.length << 1);
         }
@@ -40,11 +40,136 @@ public class AList {
         this.size++;
     }
 
-    // from A2
+    /*
+    Exercise II: Array List with Generic Types
+    You will implement an array list class class AList<T> capable of storing elements of a generic type T, similar to
+    what you've read in the self-study lesson. Your implementation should be complete with the same set of convenient
+    methods as the SLList from last time.
+
+    The general idea is to change int[] items to a generic T[] items. But there are some technical concerns.
+
+    How to make an array of generics?
+    It is not possible to write new T[2]. Java simply doesn't allow that (more on this soon). To sidestep Java's
+    craziness, you'll create an Object array and typecast it as T[]. This will give you a warning, but you can safely
+    ignore it. In effect, if you want to create an array of type T of size n and store it as items, you'll write:
+
+    items = (T[]) new Object[n];
+
+    To explain this a bit more, the expression new Object[n] makes an array of length n of type Object. This is a hack
+    because we aren't interested in storing Objects, but it is necessary and one of the dark corners of Java. The
+    expression (T[]) is known as typecasting—the process of forcing a type into another type. In this case, force the
+    thing to the right to be seen as a (T[])—that is, an array of generic type T. The joy of garbage collection When you
+    implement removeLast, it is important that you set the removed item to null, like so:
+
+
+    public T removeLast() {
+        T itemToRemove = items[size - 1];
+        items[size - 1] = null; // important: see below
+        size--;
+
+        return itemToRemove
+    }
+    While zeroing out the removed position was optional in the int array list implementation, it is crucial in the
+    generic implementation. Here are a few things to understand:
+
+    You don't explicitly free unused memory in Java. Java destroys unused objects automatically—and it knows this
+    because the last reference to an object has been lost. Therefore, if you keep a reference to an otherwise unused
+    object, Java will continue to keep this object around. We need to "free" this reference, so Java can properly
+    collect garbage!
+    */
+
+    public void addLast(T x) {
+        if (this.items.length == this.size()) {
+            resize(this.items.length << 1);
+        }
+
+        this.items[size] = x;
+        this.size++;
+    }
+
+    public T getFirst() {
+        if (this.isEmpty()) {
+            throw new IllegalStateException("empty list");
+        }
+
+        return this.items[0];
+    }
+
+    public T getLast() {
+        if (this.isEmpty()) {
+            throw new IllegalStateException("empty list");
+        }
+        return this.items[this.size - 1];
+    }
+
+    public String toString() {
+        /*
+         * since java `String` is immutable, `str = str + current.head` will creates a new string every time it gets
+         * called --- so, I use string with buffer instead.
+         */
+        StringBuffer str_buff = new StringBuffer();
+
+        str_buff.append("[");
+        for (int i = 0; i < this.size(); i++) {
+            if (str_buff.length() > 1) { // ignore the first '['
+                str_buff.append(", ");
+            }
+            str_buff.append(this.items[i]);
+        }
+        str_buff.append("]");
+
+        return str_buff.toString();
+    }
+
+    public void removeFirst() {
+        if (this.isEmpty()) {
+            return;
+            // throw new IllegalStateException("empty list");
+        }
+
+        for (int i = 0; i < (this.size() - 1); i++) {
+            this.items[i] = this.items[i + 1];
+        }
+
+        this.items[size - 1] = null;
+        this.size--;
+    }
+
+    public void removeLast() {
+        if (this.isEmpty()) {
+            return;
+            // throw new IllegalStateException("empty list");
+        }
+
+        this.items[size - 1] = null;
+        this.size--;
+    }
+
+    public void insert(T newValue, int k) {
+        if (k <= 0) {
+            addFirst(newValue);
+            return;
+        }
+        if (k >= this.size) {
+            addLast(newValue);
+            return;
+        }
+
+        if (this.size == this.items.length) {
+            resize(this.items.length << 1);
+        }
+
+        for (int i = this.size; i > k; i--) { // shift right, open entry for k
+            this.items[i] = this.items[i - 1];
+        }
+        this.items[k] = newValue;
+        this.size++;
+    }
+
     @SuppressWarnings("unchecked")
     private void resize(int new_capacity) {
-        // T[] new_items = (T[]) new Object[new_capacity];
-        int[] new_items = new int[new_capacity];
+        T[] new_items = (T[]) new Object[new_capacity];
+        // int[] new_items = new int[new_capacity];
 
         for (int i = 0; i < this.size(); i++) {
             new_items[i] = this.items[i];
