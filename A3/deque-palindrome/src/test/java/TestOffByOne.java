12eb51619f543907c8086964d15260de54f8548e
orca1018 <dev@jiraroj.net>
2026-02-06 13:05:29 +0700

    assignment 3, task 1, subtask 4, sub-subtask 2: test offByOne

--- A3/deque-palindrome/src/test/java/TestOffByOne.java
+++ A3/deque-palindrome/src/test/java/TestOffByOne.java
@@ -11,5 +11,26 @@ public class TestOffByOne {
     static CharacterComparator offByOne = new OffByOne();
 
     // Your tests go here.
+    @Test
+    public void testOffByOne_true() {
+        assertTrue(offByOne.equalChars('a', 'b'));
+        assertTrue(offByOne.equalChars('b', 'a'));
+        assertTrue(offByOne.equalChars('y', 'z'));
+        assertTrue(offByOne.equalChars('1', '0'));
+    }
+
+    @Test
+    public void testOffByOne_false() {
+        assertFalse(offByOne.equalChars('a', 'c'));
+        assertFalse(offByOne.equalChars('a', 'z'));
+        assertFalse(offByOne.equalChars('0', '2'));
+    }
+
+    @Test
+    public void testOffByOne_same_char_false() {
+        assertFalse(offByOne.equalChars('a', 'a'));
+        assertFalse(offByOne.equalChars('z', 'z'));
+        assertFalse(offByOne.equalChars('0', '0'));
+    }
 }
 // */
