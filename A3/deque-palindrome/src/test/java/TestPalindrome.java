d32602252f1d6e7f78b15300f07ad247e0001e39
orca1018 <dev@jiraroj.net>
2026-02-06 11:51:05 +0700

    assignment 3, task 1, subtask 2; uncomment the test file, according to task 2's instruction

---
+++ A3/deque-palindrome/src/test/java/TestPalindrome.java
@@ -0,0 +1,23 @@
+/**
+ * IMPORTANT: Uncomment this file after you're done with the Deque interface and wordToDeque
+ */
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class TestPalindrome {
+    // You must use this palindrome, and not instantiate
+    // new Palindromes, or the autograder might be upset.
+    static Palindrome palindrome = new Palindrome();
+
+    @Test
+    public void testWordToDeque() {
+        Deque d = palindrome.wordToDeque("persiflage");
+        String actual = "";
+        for (int i = 0; i < "persiflage".length(); i++) {
+            actual += d.removeFirst();
+        }
+        assertEquals("persiflage", actual);
+    }
+}
+// */
