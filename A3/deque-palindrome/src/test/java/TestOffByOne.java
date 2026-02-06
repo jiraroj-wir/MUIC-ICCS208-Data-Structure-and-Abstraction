51c94a1ce0f33fa0be239c37f25ee8359b4067b6
orca1018 <dev@jiraroj.net>
2026-02-06 13:00:38 +0700

    assignment 3, task 1, subtask 4, sub-subtask 2: uncomment TestOffByOne

---
+++ A3/deque-palindrome/src/test/java/TestOffByOne.java
@@ -0,0 +1,15 @@
+/* IMPORTANT:
+ * Uncomment this file after you have implemented OffByOne
+ */
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class TestOffByOne {
+    // You must use this CharacterComparator and not instantiate
+    // new ones, or the autograder might be upset.
+    static CharacterComparator offByOne = new OffByOne();
+
+    // Your tests go here.
+}
+// */
