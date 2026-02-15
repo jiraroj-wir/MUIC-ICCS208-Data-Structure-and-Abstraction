170978905903815f7d7b8ccbb2192aa66675c3df
orca1018 <dev@jiraroj.net>
2026-02-15 12:18:14 +0700

    assignment 4, task 2: main part done

--- A4/histogame/src/main/java/histogram/SimpleHistogram.java
+++ A4/histogame/src/main/java/histogram/SimpleHistogram.java
@@ -1,29 +1,102 @@
 package histogram;
 
+import java.lang.Iterable;
 import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.NoSuchElementException;
+import java.util.Set;
 
 // TODO: Uncomment this and make sure to implement all the methods
 public class SimpleHistogram<DT> implements Histogram<DT>, Iterable<DT> {
+    private final LinkedHashMap<DT, Integer> map;
+    private int totalCount;
+
     // constructs an empty histogram (with no information)
-    // public SimpleHistogram() {}
+    public SimpleHistogram() {
+        this.map = new LinkedHashMap<>();
+        this.totalCount = 0;
+    }
 
     // constructs a histogram from a list of items given by the parameter items
-    // public SimpleHistogram(DT items[]) {}
+    public SimpleHistogram(DT items[]) {
+        this();
+
+        for (DT item : items) {
+            this.totalCount++;
+            if (map.containsKey(item)) {
+                map.put(item, map.get(item) + 1);
+                continue;
+            }
+
+            map.put(item, 1);
+        }
+    }
 
     // constructs a (new) histogram from an existing histogram, sharing nothing internally
-    // public SimpleHistogram(Histogram<DT> hist) {}
+    public SimpleHistogram(Histogram<DT> hist) {
+        this();
+
+        for (DT item : hist) {
+            setCount(item, hist.getCount(item));
+        }
+    }
 
     @Override
     // Returns the total frequency count of all items in the domain combined.
-    public int getTotalCount();
+    public int getTotalCount() {
+        return this.totalCount;
+    }
 
     @Override
     // Returns the frequency count of a given domain item. If invalid domain
     // item is given, return 0.
-    public int getCount(DT item);
+    public int getCount(DT item) {
+        if (!this.map.containsKey(item)) {
+            return 0;
+        }
+
+        return this.map.get(item);
+    }
 
     @Override
     // Sets the frequecy count of a given domain item. If the  domain item
     // doesn't yet exist in the domain, this will also add it to the domain.
-    public void setCount(DT item, int count);
+    public void setCount(DT item, int count) {
+        if (this.map.containsKey(item)) {
+            this.totalCount -= this.map.get(item);
+        }
+        this.map.put(item, count);
+
+        this.totalCount += count;
+    }
+
+    @Override
+    public Iterator<DT> iterator() {
+        return new histIterator();
+    }
+
+    private class histIterator implements Iterator<DT> {
+        /*
+         * NOTE: linked hash map don't iterate so well, I shouldn't use it
+         * I tried to mimic iterator implementation here by listing all the key into an array of object (tried DT[],
+         * doesn't work), then just check the index as exiting indicator.
+         */
+        private final Object[] keys = map.keySet().toArray();
+        private int index;
+
+        @Override
+        public boolean hasNext() {
+            return index < keys.length;
+        }
+
+        @Override
+        @SuppressWarnings("unchecked")
+        public DT next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+
+            return (DT)keys[index++];
+        }
+    }
 }
