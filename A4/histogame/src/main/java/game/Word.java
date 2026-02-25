d81df73f48ff6cbcd2dfe29f3d5f77fd2c34ee7b
orca1018 <dev@jiraroj.net>
2026-02-25 18:47:19 +0700

    homework 4, task 3, subtask 1: constructor for Word

--- A4/histogame/src/main/java/game/Word.java
+++ A4/histogame/src/main/java/game/Word.java
@@ -1,6 +1,23 @@
 package game;
 
+import histogram.Histogram;
+import histogram.SimpleHistogram;
+
 public class Word implements Formable<Word>, Comparable<Word> {
+    private String word;
+    private Histogram<Character> hist;
+
+    // The only constructor of the class, which takes a String representation
+    // of the word. The histogram is generated here from the given word.
+    public Word(String word) {
+        this.word = word.toLowerCase(); // case in-sensitive
+        this.hist = new SimpleHistogram<>();
+
+        for (char ch : this.word.toCharArray()) {
+            hist.setCount(ch, hist.getCount(ch) + 1);
+        }
+    }
+
     @Override
     public boolean canForm(Word other) {
         // TODO: Fix me
