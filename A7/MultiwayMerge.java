dd6ea687a5476a86463f1a00ffab83e3b2b53d7a
orca1018 <dev@jiraroj.net>
2026-03-20 17:15:39 +0700

    assignment 7, task 2, sub-task 1: add test; made Pair class static

--- A7/MultiwayMerge.java
+++ A7/MultiwayMerge.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 public class MultiwayMerge {
-    class Pair {
+    static class Pair {
         Integer value;
         int index;
 
@@ -31,5 +31,35 @@ public class MultiwayMerge {
         }
 
         return merged;
+    }
+
+    // tests
+    public static void main(String[] args) {
+        @SuppressWarnings("unchecked")
+        LinkedList<Integer>[] t1 =
+            new LinkedList[] {new LinkedList<>(Arrays.asList(1, 4, 7)), new LinkedList<>(Arrays.asList(2, 5, 8)),
+                              new LinkedList<>(Arrays.asList(3, 6, 9))};
+        assert mergeAll(t1).equals(new LinkedList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9)));
+
+        @SuppressWarnings("unchecked")
+        LinkedList<Integer>[] t2 =
+            new LinkedList[] {new LinkedList<>(Arrays.asList(1, 10)), new LinkedList<>(Arrays.asList(2, 3, 4, 5)),
+                              new LinkedList<>(Arrays.asList(6))};
+        assert mergeAll(t2).equals(new LinkedList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 10)));
+
+        @SuppressWarnings("unchecked")
+        LinkedList<Integer>[] t3 =
+            new LinkedList[] {new LinkedList<>(Arrays.asList(1, 3, 5)), new LinkedList<>(Arrays.asList(1, 3, 5)),
+                              new LinkedList<>(Arrays.asList(1, 3, 5))};
+        assert mergeAll(t3).equals(new LinkedList<>(Arrays.asList(1, 1, 1, 3, 3, 3, 5, 5, 5)));
+
+        @SuppressWarnings("unchecked")
+        LinkedList<Integer>[] t4 =
+            new LinkedList[] {new LinkedList<>(), new LinkedList<>(Arrays.asList(1, 2)), new LinkedList<>()};
+        assert mergeAll(t4).equals(new LinkedList<>(Arrays.asList(1, 2)));
+
+        @SuppressWarnings("unchecked")
+        LinkedList<Integer>[] t5 = new LinkedList[] {new LinkedList<>(), new LinkedList<>()};
+        assert mergeAll(t5).equals(new LinkedList<>());
     }
 }
