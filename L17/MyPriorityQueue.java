eb70896482e951de24061c79beaf2a7c55358250
orca1018 <dev@jiraroj.net>
2026-03-25 18:39:12 +0700

    lecture 17: poll and heapify

--- L17/MyPriorityQueue.java
+++ L17/MyPriorityQueue.java
@@ -28,6 +28,38 @@ public class MyPriorityQueue<T> {
 
     public T peek() { return this.heap[0]; }
 
+    public T poll() {
+        T root = this.heap[0];
+
+        this.heap[0] = this.heap[this.size - 1];
+        this.heap[this.size - 1] = null;
+        this.size--;
+
+        heapify(0);
+
+        return root;
+    }
+
+    // allow heapify to be public, might be useful for user?
+    public void heapify(int i) {
+        int min = i;
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+
+        if (left < this.size && this.comparator.compare(this.heap[left], this.heap[min]) < 0) {
+            min = left;
+        }
+
+        if (right < this.size && this.comparator.compare(this.heap[right], this.heap[min]) < 0) {
+            min = right;
+        }
+
+        if (min != i) {
+            swap(i, min);
+            heapify(min);
+        }
+    }
+
     private void resize(int newSize) { this.heap = Arrays.copyOf(this.heap, newSize); }
 
     private void swap(int a, int b) {
