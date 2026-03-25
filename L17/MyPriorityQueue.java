71e4d39cf9df3361d527eb3a062dc8ae0d8d3fad
orca1018 <dev@jiraroj.net>
2026-03-25 18:16:47 +0700

    lecture 17: add operation; need resize()

--- L17/MyPriorityQueue.java
+++ L17/MyPriorityQueue.java
@@ -6,8 +6,26 @@ public class MyPriorityQueue<T> {
     Comparator<T> comparator;
 
     public MyPriorityQueue(Comparator<T> cc) {
-        heap = (T[]) new Object[2 << 4];
-        size = 0;
-        comparator = cc;
+        this.heap = (T[]) new Object[2 << 4];
+        this.size = 0;
+        this.comparator = cc;
+    }
+
+    public void add(T item) {
+        this.heap[this.size] = item;
+        int i = this.size;
+        this.size++;
+
+        // similar to bubble sort, the newly added item floats up
+        while (i > 0 && this.comparator.compare(this.heap[(i - 1) / 2], this.heap[i]) > 0) { // min heap
+            swap(i, (i - 1) / 2);
+            i = (i - 1) / 2;
+        }
+    }
+
+    private void swap(int child, int parent) {
+        T temp = this.heap[child];
+        this.heap[child] = this.heap[parent];
+        this.heap[parent] = temp;
     }
 }
