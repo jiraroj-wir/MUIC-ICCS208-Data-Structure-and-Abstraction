b8860ee31248669b76f9aa34e00f7d3eb9c41e78
orca1018 <dev@jiraroj.net>
2026-03-26 22:23:00 +0700

    lecture 20: setup

---
+++ L20/BFS.java
@@ -0,0 +1,49 @@
+import java.util.*;
+
+public class BFS {
+    /*
+    1. Using the adjancency map representation you've read about in your self-study lesson, write a class class
+    UndirectedAdjMap<Vertex> that implements the UndirectedGraph<Vertex> interface (see the reading).
+    */
+
+    public interface UndirectedGraph<Vertex> {
+        int numEdges();    // How many edges?
+        int numVertices(); // How many vertices?
+        int deg(Vertex v); // Return the degree of v
+
+        // Return an iterable of vertices adjacent to v
+        Iterable<Vertex> adj(Vertex v);
+
+        // Is there an edge between u and v?
+        boolean isEdge(Vertex u, Vertex v);
+
+        // Add a vertex
+        void addVertex(Vertex v);
+
+        // Add an undirected edge (u -- v)
+        void addEdge(Vertex u, Vertex v);
+
+        // Remove a vertex and all incident edges
+        void removeVertex(Vertex v);
+
+        // Remove an edge
+        void removeEdge(Vertex u, Vertex v);
+    }
+
+    class UndirectedAdjMap<Vertex> implements UndirectedGraph<Vertex> {
+        int edges;
+        int vertices;
+        Map<Vertex, Set<Vertex>> adj;
+
+        UndirectedAdjMap() { this.adj = new HashMap<>(); }
+    }
+
+    /*
+    2. Given an undirected graph and a pair of nodes, write a method static void findShortest(UndirectedGraph<Integer>
+    G, Integer a, Integer b) that prints out the shortest path from vertex a to vertex b.
+
+    findShortest(G, 2, 1) should print 2 3 1
+
+    (Notice that the path starts from a and ends at b)
+    */
+}
