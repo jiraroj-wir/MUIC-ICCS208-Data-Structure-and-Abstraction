44de701437cd986ed8ffeddb302cd5ba0ff68447
orca1018 <dev@jiraroj.net>
2026-03-30 09:51:50 +0700

    lecture 20, optional: Jesse and Cookies

---
+++ L20/Jesse and Cookies.java
@@ -0,0 +1,70 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class Result {
+
+    /*
+     * Complete the 'cookies' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER k
+     *  2. INTEGER_ARRAY A
+     */
+
+    public static int cookies(int k, List<Integer> A) {
+        // Write your code here
+        PriorityQueue<Integer> pq = new PriorityQueue<>(A);
+
+        int moves = 0;
+        while (!pq.isEmpty() && pq.peek() < k) {
+            int first = pq.poll();
+            if (pq.isEmpty()) {
+                return -1;
+            }
+            int second = pq.poll();
+
+            int mix = first + (second << 1);
+            pq.add(mix);
+
+            moves++;
+        }
+
+        return pq.isEmpty() ? -1 : moves;
+    }
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.parseInt(firstMultipleInput[0]);
+
+        int k = Integer.parseInt(firstMultipleInput[1]);
+
+        String[] ATemp = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        List<Integer> A = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            int AItem = Integer.parseInt(ATemp[i]);
+            A.add(AItem);
+        }
+
+        int result = Result.cookies(k, A);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
